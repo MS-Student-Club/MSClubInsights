fed8a40a0067f82fe7c30e731c53dbfa59a420a8
ahassan200695 <ahassan200695@gmail.com>
2025-03-21 01:29:09 +0200

    chore: add test user seeding

--- MSClubInsights .API/Program.cs
+++ MSClubInsights .API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Identity;
+using MSClubInsights.Domain.Entities.Identity;
 using MSClubInsights.Infrastructure.DependancyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,8 +25,25 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseSwaggerUI(o => o.SwaggerEndpoint("/openapi/v1.json", "MS Club Insights API v1"));
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+    SeedUser(userManager).Wait();
+}
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+async Task SeedUser(UserManager<AppUser> userManager)
+{
+    if (await userManager.FindByEmailAsync("admin@example.com") == null)
+    {
+        var user = new AppUser { UserName = "admin", Email = "admin@example.com", EmailConfirmed = true };
+        await userManager.CreateAsync(user, "Admin@123"); // Password
+    }
+}
