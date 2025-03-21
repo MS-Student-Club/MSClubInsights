21371e12a8f4438819cc6568196e52a661a050b5
ahassan200695 <ahassan200695@gmail.com>
2025-03-21 03:09:06 +0200

    feat(api): add ArticleController with CRUD Operations

---
+++ MSClubInsights .API/Controllers/ArticleController.cs
@@ -0,0 +1,260 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSClubInsights.API.Responses;
+using MSClubInsights.Application.ServiceInterfaces;
+using MSClubInsights.Domain.Entities;
+using System.Net;
+using MSClubInsights.Shared.DTOs.Article;
+using MSClubInsights.Infrastructure.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace MSClubInsights_.API.Controllers
+{
+    [Route("api/articles")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        private readonly IArticleService _articleService;
+        public APIResponse _response;
+        private readonly AppDbContext _db;
+        public ArticleController(IArticleService articleService , AppDbContext db)
+        {
+            _articleService = articleService;
+
+            _response = new();
+
+            _db = db;
+
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetArticles()
+        {
+            try
+            {
+                _response.Data = await _articleService.GetAllAsync();
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+            return Ok(_response);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> GetArticle(int id)
+        {
+            try
+            {
+                if(id <= 0)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages =  new List<string> { "Invalid ID. ID must be greater than zero." };
+                    _response.Data = new List<string> { "No Data Retreived" };
+                    return BadRequest(_response);
+                }
+
+                var article = await _articleService.GetAsync(u => u.Id == id);
+
+                if (article == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.ErrorMessages = new List<string> { "No Article Found " };
+                    return NotFound(_response);
+                }
+
+                _response.Data = article;
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+            return Ok(_response);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> CreateArticle([FromBody] ArticleCreateDTO createDTO)
+        {
+            try
+            {
+                if (createDTO == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages = new List<string> { "Article is null" };
+                    return BadRequest(_response);
+                }
+
+                var user = await _db.Users.OrderBy(u => u.Id).FirstOrDefaultAsync();
+
+                Article article = new()
+                {
+                    ImageUrl = createDTO.ImageUrl,
+                    Title = createDTO.Title,
+                    Content = createDTO.Content,
+                    CategoryId = createDTO.CategoryId,
+                    AuthorId = user.Id,
+                    Date = DateTime.Now,
+                };
+
+                await _articleService.AddAsync(article);
+
+                _response.Data = article;
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.Created;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>() { ex.Message };
+            }
+            return StatusCode(StatusCodes.Status201Created , _response);
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> UpdateArticle(int id , [FromBody] ArticleCreateDTO updateDTO)
+        {
+            try
+            {
+                if(updateDTO == null)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+
+                    _response.IsSuccess = false;
+
+                    _response.ErrorMessages = new List<string>()
+                    {
+                        "Article is null"
+                    };
+
+                    return BadRequest(_response);
+                }
+
+                if(id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+
+                    _response.IsSuccess = false;
+
+                    _response.ErrorMessages = new List<string>()
+                    {
+                        "Invalid ID. ID must be greater than zero."
+                    };
+                }
+                var user = await _db.Users.OrderBy(u => u.Id).FirstOrDefaultAsync();
+
+                Article article = await _articleService.GetAsync(u => u.Id == id);
+
+                article.Title = updateDTO.Title;
+                article.Content = updateDTO.Content;
+                article.ImageUrl = updateDTO.ImageUrl;
+                article.CategoryId = updateDTO.CategoryId;
+                article.AuthorId = user.Id;
+                article.Date = DateTime.Now;
+
+                await _articleService.UpdateAsync(article);
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+
+                _response.IsSuccess = true;
+
+                _response.Data = article;
+
+                return StatusCode(StatusCodes.Status204NoContent, _response);
+
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+
+            return _response;
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> DeleteArticle(int id)
+        {
+            try
+            {
+                if(id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+
+                    _response.IsSuccess = false;
+
+                    _response.ErrorMessages = new List<string>()
+                    {
+                        "Invalid ID. ID must be greater than zero."
+                    };
+
+                    return BadRequest(_response);
+                }
+
+                Article article = await _articleService.GetAsync(u => u.Id == id);
+
+                if(article == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+
+                    _response.IsSuccess = false;
+
+                    return NotFound(_response);
+                }
+
+                await _articleService.DeleteAsync(article);
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+
+                _response.IsSuccess = true;
+
+                return Ok(_response);
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+
+            return _response;
+
+        }
+
+    }
+}
