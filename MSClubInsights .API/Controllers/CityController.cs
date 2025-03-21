daa9941a9e5908ed8b690c78ae00a3e4ccf400d8
ahassan200695 <ahassan200695@gmail.com>
2025-03-21 03:07:54 +0200

    feat(api): add CityController with CRUD operations

---
+++ MSClubInsights .API/Controllers/CityController.cs
@@ -0,0 +1,239 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSClubInsights.API.Responses;
+using MSClubInsights.Application.ServiceInterfaces;
+using MSClubInsights.Domain.Entities;
+using MSClubInsights.Shared.DTOs.City;
+using System.Net;
+
+namespace MSClubInsights.API.Controllers
+{
+    [Route("api/cities")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private readonly ICityService _citySevice;
+        public APIResponse _response;
+
+        public CityController(ICityService citySevice)
+        {
+            _citySevice = citySevice;
+
+            _response = new();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetCities()
+        {
+            try
+            {
+                _response.Data = await _citySevice.GetAllAsync();
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages.Add(ex.Message);
+            }
+            return Ok(_response);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> GetCity(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages = new List<string> { "Invalid ID. ID must be greater than zero." };
+                    _response.Data = new List<string> { "No Data Retreived" };
+                    return BadRequest(_response);
+                }
+
+                var city = await _citySevice.GetAsync(u => u.Id == id);
+
+                if (city == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.ErrorMessages = new List<string> { "No City Found " };
+                    return NotFound(_response);
+                }
+
+                _response.Data = city;
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+            return Ok(_response);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> CreateCity([FromBody] CityCreateDTO createDTO)
+        {
+            try
+            {
+                if (createDTO == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages = new List<string> { "City is null" };
+                    return BadRequest(_response);
+                }
+
+                City city = new()
+                {
+                    Name = createDTO.Name
+                };
+
+                await _citySevice.AddAsync(city);
+
+                _response.Data = city;
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.Created;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>() { ex.Message };
+            }
+            return StatusCode(StatusCodes.Status201Created, _response);
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> UpdateCity(int id, [FromBody] CityUpdateDTO updateDTO)
+        {
+            try
+            {
+                if (updateDTO == null)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+
+                    _response.IsSuccess = false;
+
+                    _response.ErrorMessages = new List<string>()
+                    {
+                        "City is null"
+                    };
+
+                    return BadRequest(_response);
+                }
+
+                if (id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+
+                    _response.IsSuccess = false;
+
+                    _response.ErrorMessages = new List<string>()
+                    {
+                        "Invalid ID. ID must be greater than zero."
+                    };
+                }
+
+                City city = await _citySevice.GetAsync(u => u.Id == id);
+
+                city.Name = updateDTO.Name;
+
+                await _citySevice.UpdateAsync(city);
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+
+                _response.IsSuccess = true;
+
+                _response.Data = city;
+
+                return StatusCode(StatusCodes.Status204NoContent, _response);
+
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+
+            return _response;
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> DeleteCity(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+
+                    _response.IsSuccess = false;
+
+                    _response.ErrorMessages = new List<string>()
+                    {
+                        "Invalid ID. ID must be greater than zero."
+                    };
+
+                    return BadRequest(_response);
+                }
+
+                City city = await _citySevice.GetAsync(u => u.Id == id);
+
+                if (city == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+
+                    _response.IsSuccess = false;
+
+                    return NotFound(_response);
+                }
+
+                await _citySevice.DeleteAsync(city);
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+
+                _response.IsSuccess = true;
+
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+
+            return _response;
+
+        }
+
+    }
+}
