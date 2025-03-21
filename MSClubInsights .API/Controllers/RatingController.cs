4a1b80578fefbf84c61226a0d1d15705ac91f289
ahassan200695 <ahassan200695@gmail.com>
2025-03-21 03:32:18 +0200

    feat(api): add RatingController with CRUD Operations

---
+++ MSClubInsights .API/Controllers/RatingController.cs
@@ -0,0 +1,250 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MSClubInsights.API.Responses;
+using MSClubInsights.Application.ServiceInterfaces;
+using MSClubInsights.Domain.Entities;
+using MSClubInsights.Infrastructure.DB;
+using MSClubInsights.Shared.DTOs.Category;
+using MSClubInsights.Shared.DTOs.Rating;
+using System.Net;
+
+namespace MSClubInsights.API.Controllers
+{
+    [Route("api/ratings")]
+    [ApiController]
+    public class RatingController : ControllerBase
+    {
+        private readonly IRatingService _ratingService;
+        private readonly AppDbContext _db;
+        public APIResponse _response;
+
+        public RatingController(IRatingService ratingService , AppDbContext db)
+        {
+            _ratingService = ratingService;
+
+            _response = new();
+
+            _db = db;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetRatings()
+        {
+            try
+            {
+                _response.Data = await _ratingService.GetAllAsync();
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages.Add(ex.Message);
+            }
+            return Ok(_response);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> GetRating(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages = new List<string> { "Invalid ID. ID must be greater than zero." };
+                    _response.Data = new List<string> { "No Data Retreived" };
+                    return BadRequest(_response);
+                }
+
+                var rating = await _ratingService.GetAsync(u => u.Id == id);
+
+                if (rating == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.ErrorMessages = new List<string> { "No Rating Found " };
+                    return NotFound(_response);
+                }
+
+                _response.Data = rating;
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+            return Ok(_response);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> CreateRating([FromBody] RatingCreateDTO createDTO)
+        {
+            try
+            {
+                if (createDTO == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages = new List<string> { "Rating is null" };
+                    return BadRequest(_response);
+                }
+
+                var user = await _db.Users.OrderBy(u => u.Id).FirstOrDefaultAsync();
+
+                Rating rating = new()
+                {
+                    ArticleId = createDTO.ArticleId,
+                    Value = createDTO.Value,
+                    UserId = user.Id
+                };
+
+                await _ratingService.AddAsync(rating);
+
+                _response.Data = rating;
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.Created;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>() { ex.Message };
+            }
+            return StatusCode(StatusCodes.Status201Created, _response);
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> UpdateRating(int id, [FromBody] RatingUpdateDTO updateDTO)
+        {
+            try
+            {
+                if (updateDTO == null)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+
+                    _response.IsSuccess = false;
+
+                    _response.ErrorMessages = new List<string>()
+                    {
+                        "Rating is null"
+                    };
+
+                    return BadRequest(_response);
+                }
+
+                if (id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+
+                    _response.IsSuccess = false;
+
+                    _response.ErrorMessages = new List<string>()
+                    {
+                        "Invalid ID. ID must be greater than zero."
+                    };
+                }
+
+                Rating rating = await _ratingService.GetAsync(u => u.Id == id);
+
+                rating.Value = updateDTO.Value;
+                
+
+                await _ratingService.UpdateAsync(rating);
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+
+                _response.IsSuccess = true;
+
+                _response.Data = rating;
+
+                return StatusCode(StatusCodes.Status204NoContent, _response);
+
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+
+            return _response;
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> DeleteRating(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+
+                    _response.IsSuccess = false;
+
+                    _response.ErrorMessages = new List<string>()
+                    {
+                        "Invalid ID. ID must be greater than zero."
+                    };
+
+                    return BadRequest(_response);
+                }
+
+                Rating rating = await _ratingService.GetAsync(u => u.Id == id);
+
+                if (rating == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+
+                    _response.IsSuccess = false;
+
+                    return NotFound(_response);
+                }
+
+                await _ratingService.DeleteAsync(rating);
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+
+                _response.IsSuccess = true;
+
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+
+            return _response;
+
+        }
+
+    }
+}
