4fadf237326b630a4952135dc02de9951cb7cbc4
ahassan200695 <ahassan200695@gmail.com>
2025-03-21 03:08:25 +0200

    feat(api): add TagController with CRUD operations

---
+++ MSClubInsights .API/Controllers/TagController.cs
@@ -0,0 +1,239 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSClubInsights.API.Responses;
+using MSClubInsights.Application.ServiceInterfaces;
+using MSClubInsights.Domain.Entities;
+using MSClubInsights.Shared.DTOs.Tag;
+using System.Net;
+
+namespace MSClubInsights.API.Controllers
+{
+    [Route("api/tags")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private readonly ITagService _tagService;
+        public APIResponse _response;
+
+        public TagController(ITagService tagService)
+        {
+            _tagService = tagService;
+
+            _response = new();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetTags()
+        {
+            try
+            {
+                _response.Data = await _tagService.GetAllAsync();
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages.Add(ex.Message);
+            }
+            return Ok(_response);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> GetTag(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages = new List<string> { "Invalid ID. ID must be greater than zero." };
+                    _response.Data = new List<string> { "No Data Retreived" };
+                    return BadRequest(_response);
+                }
+
+                var tag = await _tagService.GetAsync(u => u.Id == id);
+
+                if (tag == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.ErrorMessages = new List<string> { "No Tag Found " };
+                    return NotFound(_response);
+                }
+
+                _response.Data = tag;
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+            return Ok(_response);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> CreateTag([FromBody] TagCreateDTO createDTO)
+        {
+            try
+            {
+                if (createDTO == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages = new List<string> { "Tag is null" };
+                    return BadRequest(_response);
+                }
+
+                Tag tag = new()
+                {
+                    Name = createDTO.Name
+                };
+
+                await _tagService.AddAsync(tag);
+
+                _response.Data = tag;
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.Created;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>() { ex.Message };
+            }
+            return StatusCode(StatusCodes.Status201Created, _response);
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> UpdateTag(int id, [FromBody] TagUpdateDTO updateDTO)
+        {
+            try
+            {
+                if (updateDTO == null)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+
+                    _response.IsSuccess = false;
+
+                    _response.ErrorMessages = new List<string>()
+                    {
+                        "Tag is null"
+                    };
+
+                    return BadRequest(_response);
+                }
+
+                if (id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+
+                    _response.IsSuccess = false;
+
+                    _response.ErrorMessages = new List<string>()
+                    {
+                        "Invalid ID. ID must be greater than zero."
+                    };
+                }
+
+                Tag tag = await _tagService.GetAsync(u => u.Id == id);
+
+                tag.Name = updateDTO.Name;
+
+                await _tagService.UpdateAsync(tag);
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+
+                _response.IsSuccess = true;
+
+                _response.Data = tag;
+
+                return StatusCode(StatusCodes.Status204NoContent, _response);
+
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+
+            return _response;
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> DeleteTag(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+
+                    _response.IsSuccess = false;
+
+                    _response.ErrorMessages = new List<string>()
+                    {
+                        "Invalid ID. ID must be greater than zero."
+                    };
+
+                    return BadRequest(_response);
+                }
+
+                Tag tag = await _tagService.GetAsync(u => u.Id == id);
+
+                if (tag == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+
+                    _response.IsSuccess = false;
+
+                    return NotFound(_response);
+                }
+
+                await _tagService.DeleteAsync(tag);
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+
+                _response.IsSuccess = true;
+
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+
+            return _response;
+
+        }
+
+    }
+}
