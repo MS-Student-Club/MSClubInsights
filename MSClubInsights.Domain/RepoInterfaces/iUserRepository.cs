643cd081f050e8db4876d059148f6b6a49861c56
ahassan200695 <ahassan200695@gmail.com>
2025-03-28 14:18:53 +0200

    feat: add UserRepository interface and implementation

---
+++ MSClubInsights.Domain/RepoInterfaces/iUserRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MSClubInsights.Domain.Entities.Identity;
+using MSClubInsights.Shared.DTOs.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSClubInsights.Domain.RepoInterfaces
+{
+    public interface iUserRepository
+    {
+        Task<AppUser> GetUserByEmail(string email);
+
+        Task<AppUser> GetUserById(string id);
+
+        Task<AppUser> GetUserByUserName(string userName);
+
+        Task<IdentityResult> CreateUser(AppUser user , string password);
+        
+        Task<bool> CheckPassword(AppUser user, string password);
+
+        Task<IList<string>> GetUserRoles(AppUser user);
+    }
+}
---
+++ MSClubInsights.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MSClubInsights.Domain.Entities.Identity;
+using MSClubInsights.Infrastructure.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSClubInsights.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserRepository(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<AppUser> GetUserByEmail(string email)
+        {
+            return await _userManager.FindByEmailAsync(email);
+        }
+
+        public async Task<AppUser> GetUserById(string id)
+        {
+            return await _userManager.FindByIdAsync(id);
+        }
+
+        public async Task CreateUser(AppUser user , string password)
+        {
+            await _userManager.CreateAsync(user , password);
+        }
+
+        public async Task<bool> CheckPassword(AppUser user, string password)
+        {
+            return await _userManager.CheckPasswordAsync(user, password);
+        }
+
+        public async Task<IList<string>> GetUserRoles(AppUser user)
+        {
+            return await _userManager.GetRolesAsync(user);
+        }
+    }
+}
