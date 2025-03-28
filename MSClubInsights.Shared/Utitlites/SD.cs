4fcdffb02d0d887adc7e5c176d3be1570eb286c3
ahassan200695 <ahassan200695@gmail.com>
2025-03-29 00:10:25 +0200

    feat: add SD class for standard definitions and define role names

---
+++ MSClubInsights.Shared/Utitlites/SD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSClubInsights.Shared.Utitlites
+{
+    public class SD
+    {
+        public const string SysAdmin = "System Admin";
+        public const string CoreTeam = "Core Team";
+        public const string RegMember = "Regular Member";
+        public const string TechMember = "Technical Member";
+
+    }
+}
